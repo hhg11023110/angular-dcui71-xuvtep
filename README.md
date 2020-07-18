04a30cff9ebcff0a553ddeca7288af08e80461ae
hhg11023110 <413304251@qq.com>
2020-07-18 08:53:25 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-dcui71-xuvtep
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-dcui71-xuvtep)
---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService } from './hero.service';
+import { MessageService } from './message.service';
+import { MessageComponent } from './message/message.component'; // <-- NgModel lives here
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessageComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [HeroService, MessageService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
